ebed4f4d385d9c8b31897257f53e1de032739475
zameschua <zameschua@gmail.com>
2017-01-21 21:04:37 +0800

    add telegram bot package

--- server/main.js
+++ server/main.js
@@ -10,4 +10,21 @@ Meteor.startup(() => {
   		});
 	}
 
+  	// set telegram token
+    TelegramBot.token = '309396508:AAHdsqT9dlHFxdYl3QIoo0W2FTjuooPAJG8';
+    console.log(TelegramBot);
+    TelegramBot.start(); // start the bot
+    // add a listener for '/test'
+    TelegramBot.addListener('/test', function(command, username, original) {
+        console.log(username);
+        console.log(original);
+        if(!command[1]) { // if no arguments
+            return false
+            // if you return false the bot wont answer
+        }
+        // command[1] will be the first argument, command[2] the second etc
+        // below the bot will reply with 'test: hi' if you sent him /test hi
+        return "test: " + command[1]
+    });
+
 });
